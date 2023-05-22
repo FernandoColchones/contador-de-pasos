498aa3a9a22d2cf1fbde5963f8306e0867f9c0aa
FernandoColchones <130452951+FernandoColchones@users.noreply.github.com>
2023-05-22 09:13:30 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-input.onButtonPressed(Button.A, function () {
-    pasos = 0
-})
-input.onGesture(Gesture.Shake, function () {
-    pasos += 1
-    basic.showNumber(pasos)
-})
 let pasos = 0
-pasos = 0
+basic.showNumber(0)
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        pasos += 1
+        basic.showNumber(pasos)
+    }
+})
