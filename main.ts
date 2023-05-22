ece842fe364bca89ace0dd31e4cdbac22270870f
FernandoColchones <130452951+FernandoColchones@users.noreply.github.com>
2023-05-22 09:32:04 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(pasos)
+})
+input.onButtonPressed(Button.AB, function () {
+    pasos = 49 * pasos
+})
+input.onButtonPressed(Button.B, function () {
+    pasos = 0
+})
 let pasos = 0
+pasos = 0
 basic.showNumber(0)
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 1500) {
         pasos += 1
-        basic.showNumber(pasos)
     }
 })
